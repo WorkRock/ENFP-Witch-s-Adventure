afbbb7b19333d717c6dd000f08540f9feb15022b
LeeJiHun <wbsrl3532@naver.com>
2022-08-16 15:08:33 +0900

    Singleton,[Score,Sound]Manager,Resources

---
+++ ENFPWA/Assets/06.Controllers/ScoreManager.cs
@@ -0,0 +1,183 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : Singleton<ScoreManager>
+{   
+    protected int PlayerLevel;
+    protected int AtkUG;
+    protected int Coin;
+    protected int Exp;
+    protected int BStage;
+    protected int Stage;
+    protected bool IsLobby; // 로비 화면인지를 체크
+    protected bool IsSoundOn; // 소리가 켜져있는지 체크
+    protected bool IsDragonDie; // 드래곤이 현재 사망상태인지 체크
+
+
+    protected int PlayerHP;
+    protected float PlayerTotalAtk;
+    protected float ShieldCT;
+
+    public ScoreManager()
+    {
+        // ScoreManager 초기화
+    }
+
+
+    // Get 메소드
+    public static int GetPlayerLevel()
+    {
+        return GetInstance().PlayerLevel;
+    }
+
+    public static int GetAtkUG()
+    {
+        return GetInstance().AtkUG;
+    }
+
+    public static int GetCoin()
+    {
+        return GetInstance().Coin;
+    }
+
+    public static int GetExp()
+    {
+        return GetInstance().Exp;
+    }
+
+    public static int GetBStage()
+    {
+        return GetInstance().BStage;
+    }
+
+    public static int GetStage()
+    {
+        return GetInstance().Stage;
+    }
+
+    public static bool GetIsLobby()
+    {
+        return GetInstance().IsLobby;
+    }
+
+    public static bool GetIsSoundOn()
+    {
+        return GetInstance().IsSoundOn;
+    }
+
+    public static bool GetIsDragonDie()
+    {
+        return GetInstance().IsDragonDie;
+    }
+
+    public static int GetPlayerHP()
+    {
+        return GetInstance().PlayerHP;
+    }
+
+    public static float GetPlayerTotalAtk()
+    {
+        return GetInstance().PlayerTotalAtk;
+    }
+
+    public static float GetShieldCT()
+    {
+        return GetInstance().ShieldCT;
+    }
+
+
+    // Set 메소드
+    public static void SetPlayerLevel(int _PlayerLevel)
+    {
+        GetInstance().PlayerLevel = _PlayerLevel;
+        PlayerPrefs.SetInt("Level",_PlayerLevel);
+        PlayerPrefs.Save();
+    }
+
+    public static void SetAtkUG(int _AtkUG)
+    {
+        GetInstance().AtkUG = _AtkUG;
+        PlayerPrefs.SetInt("AtkUG",_AtkUG);
+        PlayerPrefs.Save();
+    }
+
+    public static void SetCoin(int _Coin)
+    {
+        GetInstance().Coin = _Coin;
+        PlayerPrefs.SetInt("Coin",_Coin);
+        PlayerPrefs.Save();
+    }
+
+    public static void SetExp(int _Exp)
+    {
+        GetInstance().Exp = _Exp;
+        PlayerPrefs.SetInt("Exp",_Exp);
+        PlayerPrefs.Save();
+    }
+
+    public static void SetBStage(int _BStage)
+    {
+        GetInstance().BStage = _BStage;
+        PlayerPrefs.SetInt("BStage",_BStage);
+        PlayerPrefs.Save();
+    }
+
+    public static void SetStage(int _Stage)
+    {
+        GetInstance().Stage = _Stage;
+    }
+
+    public static void SetIsLobby(bool _IsLobby)
+    {
+        GetInstance().IsLobby = _IsLobby;
+    }
+
+    public static void SetIsSoundOn(bool _IsSoundOn)
+    {
+        GetInstance().IsSoundOn = _IsSoundOn;
+    }
+
+    public static void SetIsDragonDie(bool _IsDragonDie)
+    {
+        GetInstance().IsDragonDie = _IsDragonDie;
+    }
+
+    public static void SetPlayerHP(int _PlayerHP)
+    {
+        GetInstance().PlayerHP = _PlayerHP;
+    }
+
+    public static void SetPlayerTotalAtk(float _PlayerTotalAtk)
+    {
+        GetInstance().PlayerTotalAtk = _PlayerTotalAtk;
+    }
+
+    public static void SetShieldCT(float _ShieldCT)
+    {
+        GetInstance().ShieldCT = _ShieldCT;
+    }
+
+    public static int totalIntFormula(int nowStageLv, int BasicPlus, int EditDef, int EditPlus, int BasicCor, int EditCor)
+    {
+        int cal = 0;
+        if(nowStageLv <= 1)
+            return cal;
+
+        cal = ((nowStageLv / BasicCor) * BasicPlus) + EditDef + ((nowStageLv / EditCor) * EditPlus);
+
+        return cal;
+    }
+
+    public static float totalFloatFormula(int nowStageLv, float BasicPlus, float EditDef, float EditPlus, int BasicCor, int EditCor)
+    {
+        float cal = 0;
+        if(nowStageLv <= 1)
+            return cal;
+
+        cal = ((nowStageLv / BasicCor) * BasicPlus) + EditDef + ((nowStageLv / EditCor) * EditPlus);
+
+        return cal;
+    }
+
+}
---
+++ ENFPWA/Assets/06.Controllers/SoundManager.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    /* // 싱글톤 //
+     * instance라는 변수를 static으로 선언을 하여 다른 오브젝트 안의 스크립트에서도 instance를 불러올 수 있게 합니다 
+     */
+    public static SoundManager instance = null; 
+
+    private void Awake()
+    {
+        if (instance == null) //instance가 null. 즉, 시스템상에 존재하고 있지 않을때
+        {
+            instance = this; //내자신을 instance로 넣어줍니다.
+            DontDestroyOnLoad(gameObject); //OnLoad(씬이 로드 되었을때) 자신을 파괴하지 않고 유지
+        }
+        else
+        {
+            if (instance != this) //instance가 내가 아니라면 이미 instance가 하나 존재하고 있다는 의미
+                Destroy(this.gameObject); //둘 이상 존재하면 안되는 객체이니 방금 AWake된 자신을 삭제
+        }
+    }
+
+
+    [System.Serializable]
+    public struct AudioType
+    {
+        public string name;
+        public AudioClip audio; 
+    }
+
+    // Inspector 에표시할 사운드 목록
+    public AudioType[] BGMList; // BGM 목록
+
+    public AudioType[] AudioList; // 효과음 목록
+
+    private AudioSource BGM;
+    private AudioSource audioSource_01;
+    private AudioSource audioSource_02;
+    private AudioSource audioSource_03;
+    private AudioSource audioSource_04;
+    private AudioSource audioSource_05;
+
+    private string NowBGMname="";
+
+    void Start()
+    {
+        BGM = gameObject.AddComponent<AudioSource>();
+        audioSource_01 = gameObject.AddComponent<AudioSource>();
+        audioSource_02 = gameObject.AddComponent<AudioSource>();
+        audioSource_03 = gameObject.AddComponent<AudioSource>();
+        audioSource_04 = gameObject.AddComponent<AudioSource>();
+        audioSource_05 = gameObject.AddComponent<AudioSource>();
+        BGM.loop = true;
+    }
+    
+    public void PlayBGM(string name)
+    {
+        if (NowBGMname.Equals(name)) return;
+
+        for (int i = 0; i < BGMList.Length; ++i)
+            if (BGMList[i].name.Equals(name))
+            {
+                BGM.clip = BGMList[i].audio;
+                BGM.Play();
+                NowBGMname = name;
+            }
+    }
+
+    public void PlayAudio_01(string name)
+    {
+        for (int i = 0; i < AudioList.Length; ++i)
+            if (AudioList[i].name.Equals(name))
+            {
+                audioSource_01.clip = AudioList[i].audio;
+                audioSource_01.Play();
+            }
+    }
+
+     public void PlayAudio_02(string name)
+    {
+        for (int i = 0; i < AudioList.Length; ++i)
+            if (AudioList[i].name.Equals(name))
+            {
+                audioSource_02.clip = AudioList[i].audio;
+                audioSource_02.Play();
+            }
+    }
+
+     public void PlayAudio_03(string name)
+    {
+        for (int i = 0; i < AudioList.Length; ++i)
+            if (AudioList[i].name.Equals(name))
+            {
+                audioSource_03.clip = AudioList[i].audio;
+                audioSource_03.Play();
+            }
+    }
+
+     public void PlayAudio_04(string name)
+    {
+        for (int i = 0; i < AudioList.Length; ++i)
+            if (AudioList[i].name.Equals(name))
+            {
+                audioSource_04.clip = AudioList[i].audio;
+                audioSource_04.Play();
+            }
+    }
+
+     public void PlayAudio_05(string name)
+    {
+        for (int i = 0; i < AudioList.Length; ++i)
+            if (AudioList[i].name.Equals(name))
+            {
+                audioSource_05.clip = AudioList[i].audio;
+                audioSource_05.Play();
+            }
+    }
+
+}
